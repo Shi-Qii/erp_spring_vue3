28fa28c59434946d0d85e8311aaffcefe4fc31f6
shiqi <shiqi@mitake.com.tw>
2022-09-13 15:14:49 +0800

    git init

---
+++ src/main/java/com/example/erp_spring/ErpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.erp_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/erp_spring/ErpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erp_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
